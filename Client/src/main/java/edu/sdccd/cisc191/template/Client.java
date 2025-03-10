b4dde9aec93855dcfd7af4b0baa2ff42cfa71e62
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-03-10 13:21:01 -0700

    mocking server response for UI elements

--- Client/src/main/java/edu/sdccd/cisc191/template/Client.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Client.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * This program opens a connection to a computer specified
@@ -31,36 +32,36 @@ import java.net.Socket;
  */
 
 public class Client extends Application {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
-    public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    }
-
-    public Game[] sendRequest() throws Exception {
-        out.println(CustomerRequest.toJSON(new CustomerRequest(1)));
-        return new Game[]{Game.fromJSON(in.readLine())};
-    }
-
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-    }
-    public Game[] accessServer() {
-        Client client = new Client();
-        try {
-            client.startConnection("127.0.0.1", 4444);
-            return client.sendRequest();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    private Socket clientSocket;
+//    private PrintWriter out;
+//    private BufferedReader in;
+//
+//    public void startConnection(String ip, int port) throws IOException {
+//        clientSocket = new Socket(ip, port);
+//        out = new PrintWriter(clientSocket.getOutputStream(), true);
+//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//    }
+//
+//    public Game[] sendRequest() throws Exception {
+//        out.println(CustomerRequest.toJSON(new CustomerRequest(1)));
+//        return new Game[]{Game.fromJSON(in.readLine())};
+//    }
+//
+//    public void stopConnection() throws IOException {
+//        in.close();
+//        out.close();
+//        clientSocket.close();
+//    }
+//    public Game[] accessServer() {
+//        Client client = new Client();
+//        try {
+//            client.startConnection("127.0.0.1", 4444);
+//            return client.sendRequest();
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
     public static void main(String[] args) {
         launch();  // Run this Application.
@@ -69,41 +70,45 @@ public class Client extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        HBox labelView = new HBox();
+        try {
+            // Mock response
+            Game[] response = new Game[]{
+                    new Game("Team 1", "Team 2", new Date(2025, 2, 24)),
+                    new Game("Team 3", "Team 4", new Date(2025, 2, 25)),
+                    new Game("Team 5", "Team 6", new Date(2025, 2, 26))
+            };
 
-        Game[] response = accessServer();
+            // Iterate over the response and create new Labels
+            for (Game game : response) {
+                Label gameLabel = new Label();
+                gameLabel.setText(game.toString());
+                gameLabel.setFont(Font.font("Arial", 20));
+                labelView.getChildren().add(gameLabel); // Add the new Label
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Label msg1 = new Label("Betting App");
-        Label msg2 = new Label("Betting App");
-        Label msg3 = new Label("Betting App");
 
-        msg1.setFont(new Font(40));
-        msg2.setFont(new Font(40));
-        msg3.setFont(new Font(40));
+//        Button helloButton = new Button("Vs");
+//        helloButton.setOnAction(evt -> {
+//            msg1.setText(response[0].getTeam1() + " Vs " + response[0].getTeam2());
+//            msg2.setText(response[1].getTeam1() + " Vs " + response[1].getTeam2());
+//            msg3.setText(response[2].getTeam1() + " Vs " + response[2].getTeam2());
+//                });Ω
+//        Button goodbyeButton = new Button("Date");
+//        goodbyeButton.setOnAction(evt -> {
+//            msg1.setText(response[0].getDate().toString());
+//            msg2.setText(response[0].getDate().toString());
+//            msg3.setText(response[0].getDate().toString());
+//        });
+//        Button quitButton = new Button("Quit");
+//        quitButton.setOnAction( evt -> Platform.exit() );
 
-
-        Button helloButton = new Button("Vs");
-        helloButton.setOnAction(evt -> {
-            msg1.setText(response[0].getTeam1() + " Vs " + response[0].getTeam2());
-            msg2.setText(response[1].getTeam1() + " Vs " + response[1].getTeam2());
-            msg3.setText(response[2].getTeam1() + " Vs " + response[2].getTeam2());
-                });
-        Button goodbyeButton = new Button("Date");
-        goodbyeButton.setOnAction(evt -> {
-            msg1.setText(response[0].getDate().toString());
-            msg2.setText(response[0].getDate().toString());
-            msg3.setText(response[0].getDate().toString());
-        });
-        Button quitButton = new Button("Quit");
-        quitButton.setOnAction( evt -> Platform.exit() );
-
-        HBox buttonBar = new HBox();
-        buttonBar.getChildren().addAll(helloButton, goodbyeButton, quitButton);
-        buttonBar.setAlignment(Pos.CENTER);
+        labelView.setAlignment(Pos.CENTER);
         BorderPane root = new BorderPane();
-        root.setTop(msg1);
-        root.setCenter(msg2);
-        root.setLeft(msg3);
-        root.setBottom(buttonBar);
+//        root.setBottom(buttonBar);
 
         Scene scene = new Scene(root, 450, 200);
         stage.setScene(scene);
