c0700228043733af291229d6db8fc49a9769e3e3
cmall <chaseallanxd@gmail.com>
2025-03-23 21:00:16 -0700

    unfinished bettest with errors and I dont even know if I am doing the right thing

--- Common/src/test/java/edu/sdccd/cisc191/template/BetTest.java
+++ Common/src/test/java/edu/sdccd/cisc191/template/BetTest.java
@@ -2,5 +2,31 @@ package edu.sdccd.cisc191.template;
 
 public class BetTest {
 
+   import java.util.Random;
+
+    public class BettingOddsAndTime {
+
+        public static void main(String[] args) {
+            int rows = 5;
+            int columns = 2;
+
+            int[][] data = new int[rows][columns];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < rows; i++) {
+                data[i][0] = rand.nextInt(100) + 1;
+
+                data[i][1] = rand.nextInt(24) + 1;
+            }
+
+            System.out.println("Betting Odds and Time:");
+            for (int i = 0; i < rows; i++) {
+                System.out.println("Betting Odds: " + data[i][0] + ", Time: " + data[i][1] + " hours");
+            }
+        }
+    }
+
+}
     // TODO: Test historical betting odds 2D array
 }
